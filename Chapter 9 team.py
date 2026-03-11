78648669fb25b0de1e402f56406a6a63a1129c2a
Bryson-dev <flapjackroblox@gmail.com>
2026-03-11 11:54:03 -0500

    Update Chapter 9 team.py

---
+++ Chapter 9 team.py
@@ -0,0 +1,43 @@
+def main():
+    #main accepts no arguments
+    #Main calls all other functions to build the entire program
+    #it outputs the function the user selects
+    pass
+    
+    
+def get_shift():
+    #Get shift accepts no argumnets
+    #Prompts the user for the shift value
+    #returns the value as a string
+    pass
+    
+def choose_options():
+    #Choose option accepts no arguments
+    #Choose option prompts the user to choose encode or decode
+    #reutrns True if the user chooses encode and False if the user chooses decode
+    pass
+    
+    
+def get_message():
+    #get message accepts no arguments
+    #get message prompts the user to enter a message to encode or decode
+    #it should return that message
+    pass
+
+def create_key(shift):
+    #Create key accepts an argumnet for shift
+    #It creates the caesar cipher according to the shift value and store in key
+    #it returns the dictionary as the key
+    pass
+
+def encode(message, key):
+    #encode accepts an argument for message and key
+    #it should encode the message using the key
+    #returns the encoded message
+    pass
+
+def decode(message, key):
+    #decode accepts an argument for message and key
+    #it should decode the message using the key
+    #returns the decoded message
+    pass
