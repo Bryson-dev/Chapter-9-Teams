ed433fe38b8feb69dda2c0139977cc1944f15596
Bryson-dev <flapjackroblox@gmail.com>
2026-03-26 13:04:25 -0500

    Update Chapter 9 team.py

--- Chapter 9 team.py
+++ Chapter 9 team.py
@@ -1,45 +1,47 @@
-def main():
+def main(): #bricen
     #main accepts no arguments
     #Main calls all other functions to build the entire program
     #it outputs the function the user selects
     pass
     
     
-def get_shift():
+def get_shift(): #bricen
     #Get shift accepts no argumnets
     #Prompts the user for the shift value
     #returns the value as a string
-    pass
+    key = True
+    while key == True:
+        print("NIGGER")
+
     
-def choose_options():
+def choose_options(): #bricen
     #Choose option accepts no arguments
     #Choose option prompts the user to choose encode or decode
     #reutrns True if the user chooses encode and False if the user chooses decode
     pass
     
     
-def get_message():
+def get_message(): #bricen
     #get message accepts no arguments
     #get message prompts the user to enter a message to encode or decode
     #it should return that message
     pass
 
-def create_key(shift):
+def create_key(shift): #Bryson
     #Create key accepts an argumnet for shift
     #It creates the caesar cipher according to the shift value and store in key
     #it returns the dictionary as the key
     pass
 
-def encode(message, key):
+def encode(message, key): #Bryson
     #encode accepts an argument for message and key
     #it should encode the message using the key
     #returns the encoded message
     pass
 
-def decode(message, key):
+def decode(message, key): #Bryson
     #decode accepts an argument for message and key
     #it should decode the message using the key
     #returns the decoded message
     pass
-
 main()
